6480b75b125dae567a4c7c16be7808b5ac51a1f4
Andrea-Miele <72763162+Andrea-Miele@users.noreply.github.com>
2023-09-10 16:08:50 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_j6lte-user
-add_lunch_combo omni_j6lte-userdebug
-add_lunch_combo omni_j6lte-eng
+add_lunch_combo twrp_j6lte-user
+add_lunch_combo twrp_j6lte-userdebug
+add_lunch_combo twrp_j6lte-eng
